d1f38dbe3149372363bc30bc5f053c05415715b0
rescarabel0 <r.escarabelo@gmail.com>
2022-12-11 09:58:10 -0300

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LandingPageComponent} from "./components/landing-page/landing-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "/", children: [
+      {path: "", pathMatch: "full", component: LandingPageComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
